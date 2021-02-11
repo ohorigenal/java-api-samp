68466f034691c0195b7e9fc80ac6da6a0aeb56e0
ohorigenal <6256coo@gmail.com>
2021-02-12 01:44:18 +0900

    add Exception Handler

---
+++ src/main/java/com/compass/javaapisamp/controller/APIControllerExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.compass.javaapisamp.controller;
+
+import com.compass.javaapisamp.model.dto.ErrorResponse;
+import com.compass.javaapisamp.model.exception.APIException;
+import com.compass.javaapisamp.model.exception.Errors;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Slf4j
+@RestControllerAdvice
+public class APIControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> exceptionHandler(Exception e) {
+        log.error(e.getMessage());
+
+        return new ResponseEntity<>(
+                new ErrorResponse(Errors.INTERNAL_SERVER_ERROR.getMessage()),
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+
+    @ExceptionHandler(APIException.class)
+    public ResponseEntity<ErrorResponse> exceptionHandler(APIException e) {
+        if(e.getError().getStatus().is4xxClientError()) {
+            log.debug(e.getMessage());
+        }
+        return new ResponseEntity<>(
+                new ErrorResponse(e.getError().getMessage()),
+                e.getError().getStatus()
+        );
+    }
+
+    /*
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return super.handleExceptionInternal(ex, body, headers, status, request);
+    }*/
+}
---
+++ src/main/java/com/compass/javaapisamp/model/dto/ErrorResponse.java
@@ -0,0 +1,8 @@
+package com.compass.javaapisamp.model.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ErrorResponse {
+    private final String message;
+}
