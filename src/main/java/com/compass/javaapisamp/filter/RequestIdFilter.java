7f1f8b2885e112c2f2846fe700a063fac522ceaa
ohorigenal <6256coo@gmail.com>
2021-02-16 11:48:21 +0900

    add request id filter

---
+++ src/main/java/com/compass/javaapisamp/filter/RequestIdFilter.java
@@ -0,0 +1,35 @@
+package com.compass.javaapisamp.filter;
+
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class RequestIdFilter extends OncePerRequestFilter {
+
+    private static final String ID_KEY = "ID";
+    private static final String X_HEADER = "X-Request-ID";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String requestId = request.getHeader(X_HEADER);
+        if(!StringUtils.hasText(requestId)) {
+            requestId = UUID.randomUUID().toString();
+        }
+        try {
+            MDC.put(ID_KEY, requestId);
+            response.addHeader(X_HEADER, requestId);
+            filterChain.doFilter(request, response);
+        }finally {
+            MDC.remove(ID_KEY);
+        }
+    }
+}
