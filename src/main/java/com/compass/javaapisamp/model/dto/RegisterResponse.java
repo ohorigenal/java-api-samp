1c50dc5db4e25909e7aa7391d669a82e0daad24a
ohorigenal <6256coo@gmail.com>
2021-02-17 14:13:09 +0900

    mod RegisterResponse

--- src/main/java/com/compass/javaapisamp/model/dto/RegisterResponse.java
+++ src/main/java/com/compass/javaapisamp/model/dto/RegisterResponse.java
@@ -1,8 +1,10 @@
 package com.compass.javaapisamp.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 public class RegisterResponse {
+    @JsonProperty("message")
     private final String message = "weather registered.";
 }
