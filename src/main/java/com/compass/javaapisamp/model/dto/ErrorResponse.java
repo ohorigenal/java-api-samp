4cac03bc5f934dc7311e6c4afa9e56819ad06760
ohorigenal <6256coo@gmail.com>
2021-02-18 00:18:22 +0900

    mod ErrorResponse add JsonProperty

--- src/main/java/com/compass/javaapisamp/model/dto/ErrorResponse.java
+++ src/main/java/com/compass/javaapisamp/model/dto/ErrorResponse.java
@@ -1,8 +1,10 @@
 package com.compass.javaapisamp.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class ErrorResponse {
+    @JsonProperty("message")
     private final String message;
 }
