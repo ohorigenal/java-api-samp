7e8e528841fd48b40555693f2e241ec3a1243014
ohorigenal <6256coo@gmail.com>
2021-02-22 20:53:54 +0900

    add LocationMangerTest

---
+++ src/test/java/com/compass/javaapisamp/repository/LocationManagerTest.java
@@ -0,0 +1,32 @@
+package com.compass.javaapisamp.repository;
+
+import com.compass.javaapisamp.model.entity.Location;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+public class LocationManagerTest {
+
+    @Autowired
+    private LocationManager locationManager;
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, 東京",
+            "2, 四谷",
+    })
+    @Sql(value = "classpath:/sql/location/insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    void findById(int id, String city) {
+        Optional<Location> actual = locationManager.findById(id);
+        assertEquals(new Location(id, city), actual.orElseThrow());
+    }
+
+}
