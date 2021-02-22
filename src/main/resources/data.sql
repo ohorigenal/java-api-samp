88fe1c38e2e4341dfb2b12b79e5f318592815165
ohorigenal <6256coo@gmail.com>
2021-02-22 20:52:35 +0900

    mod data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,31 +1,24 @@
-DROP DATABASE IF EXISTS javaapi;
-CREATE DATABASE IF NOT EXISTS javaapi;
-USE goapi;
-
-DROP TABLE IF EXISTS WEATHER;
-CREATE TABLE IF NOT EXISTS WEATHER
-(
-dat char(8) NOT NULL,
-weather int NOT NULL,
-location_id int NOT NULL,
-comment varchar(255),
-PRIMARY KEY(dat, location_id)
-);
-
-INSERT INTO WEATHER
-(
-dat, weather, location_id, comment
-)
-VALUES
-("20200101", 1, 1, "test comment");
-
-DROP TABLE IF EXISTS LOCATION;
 CREATE TABLE IF NOT EXISTS LOCATION
 (
-id int PRIMARY KEY,
+id int AUTO_INCREMENT PRIMARY KEY,
 city varchar(20) NOT NULL
 );
 
-INSERT INTO LOCATION VALUES
-(1, "新宿"),
-(2, "中野");
+INSERT INTO LOCATION(id, city) VALUES
+(1, '新宿'),
+(2, '中野');
+
+CREATE TABLE IF NOT EXISTS WEATHER
+(
+dat char(8),
+weather int NOT NULL,
+location_id int NOT NULL,
+comment varchar(255),
+PRIMARY KEY(dat, location_id),
+FOREIGN KEY(location_id) REFERENCES LOCATION(id)
+);
+
+INSERT INTO WEATHER
+(dat, weather, location,location_id, comment) VALUES
+('20200101', 1, 1, 1, 'test comment'),
+('20200111', 1, 2, 2, 'test comment2');
